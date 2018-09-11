29636f5baa36c0dc1e7508ae36f34d0b1f200a19
songyunqi <songyunqi@qianmi.com>
2018-09-11 10:47:32 +0800

    update

--- src/main/java/com/foo/base/user/UserRequest.java
+++ src/main/java/com/foo/base/user/UserRequest.java
@@ -2,10 +2,12 @@ package com.foo.base.user;
 
 import com.foo.base.request.ARequest;
 import lombok.Data;
+import org.hibernate.validator.constraints.NotBlank;
 
 @Data
 public class UserRequest extends ARequest {
 
+    @NotBlank
     private String name;
 
 }
