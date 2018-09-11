962da2398e7ed40d566ab818ce80b381ac01a57f
songyunqi <songyunqi@qianmi.com>
2018-09-11 10:22:22 +0800

    update

--- src/main/java/com/foo/base/response/AResponse.java
+++ src/main/java/com/foo/base/response/AResponse.java
@@ -1,32 +1,12 @@
 package com.foo.base.response;
 
+import lombok.Data;
+
+@Data
 public class AResponse<T> {
 
-	private T content;
-	private int status = 0;
-	private String msg = "0";
+    private T content;
+    private int status = 0;
+    private String msg = "0";
 
-	public T getContent() {
-		return content;
-	}
-
-	public void setContent(T content) {
-		this.content = content;
-	}
-
-	public String getMsg() {
-		return msg;
-	}
-
-	public void setMsg(String msg) {
-		this.msg = msg;
-	}
-
-	public int getStatus() {
-		return status;
-	}
-
-	public void setStatus(int status) {
-		this.status = status;
-	}
 }
